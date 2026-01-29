ee76b2cd61511b49170faabbf1069b698449f971
Industreer <j.irrgang@industreer.de>
2026-01-29 14:52:39 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4585,7 +4585,6 @@ app.get("/api/admin/invoices/:id.pdf", async (req, res) => {
     doc.fillColor("black");
 
     doc.end();
-  } 
 catch (e) {
     console.error("PDF ERROR:", e);
     return res.status(500).send("PDF Fehler: " + (e.message || ""));
