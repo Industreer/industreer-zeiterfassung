fdfb7e4d286c13000be6b8db40b24a114f4bd4b1
Industreer <j.irrgang@industreer.de>
2026-02-11 13:03:48 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,7 @@ const { Pool } = require("pg");
 const { downloadExcelFromShareLink } = require("./sharepoint");
 const { buildErfassungsbogenPdf } = require("./a10/erfassungsbogenPdf");
 const { loadStaffplanMapping } = require("./lib/staffplanProjectMapping");
-
-
+const { applyStaffplanToRows } = require("./lib/applyStaffplanToRows");
 
 const app = express();
 // ======================================================
